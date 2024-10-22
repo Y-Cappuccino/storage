c322e81927ebb938d97f09997385113f6c2543aa
Aurelien Pisu <aurelien.pisu@araymond.com>
2024-10-22 17:33:39 +0200

    wip

--- src/unittest/python/crud_model_type.py
+++ src/unittest/python/crud_model_type.py
@@ -1,4 +1,28 @@
 """
-test that alloow to cread, read , update deleet a model type using different reposutory base (memory, file, database)
-and create model under model type
+test that alloow to cread, read , update deleet a item item using different reposutory base (memory, file, database)
+and create item under item item
 """
+
+
+class TestCreateItemType:
+
+    async def setup(self):
+        pass
+
+    async def test_no_item_when_create_new_item_then_item_is_available(self):
+        pass
+
+    async def test_item_when_get_item_then_item_exists(self):
+        pass
+
+    async def test_no_item_when_create_item_then_no_item_exists(self):
+        pass
+
+    async def test_item_when_create_item_already_exists_then_error(self):
+        pass
+
+    async def test_item_when_delete_item_then_item_deleted(self):
+        pass
+
+    async def test_item_when_update_item_then_item_updated(self):
+        pass
