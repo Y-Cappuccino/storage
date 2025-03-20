a705278bd1372c9e759360c8a146f536a37b6d1c
Aurelien Pisu <aurelien.pisu@araymond.com>
2025-03-20 18:21:52 +0100

    start add test

--- src/main/python/ycappuccino/storage/adapter/pymongo_storage_adapter.py
+++ src/main/python/ycappuccino/storage/adapter/pymongo_storage_adapter.py
@@ -51,6 +51,11 @@ class PyMongoStorageAdapter(IStorage):
 
         self._password = "ycappuccino"
         self._db_name = "ycappuccino"
+        self._client = None
+
+    async def connect(self):
+        self._client = self._mongo_client_type(self._host, int(self._port))
+        self._db = self._client[self._db_name]
 
     async def load_configuration(self):
         self._host = self._config.get("storage.mongo.db.host", "localhost")
@@ -61,13 +66,15 @@ class PyMongoStorageAdapter(IStorage):
         self._password = self._config.get("storage.mongo.db.password", "ycappuccino")
         self._db_name = self._config.get("storage.mongo.db.name", "ycappuccino")
 
-    async def aggregate(self, a_collection: str, a_pipeline: Query):
+    async def aggregate(
+        self, a_collection: str, a_pipeline: t.List[t.Dict[str, t.Any]]
+    ):
         """aggegate data regarding filter and pipeline"""
         pass
         # return self._db[a_collection].aggregate(a_pipeline)
 
     async def get_one(
-        self, a_collection: str, a_filter: Filter, a_params: Query = None
+        self, a_collection: str, a_filter: Filter = None, a_params: Query = None
     ):
         """get dict identify by a Id"""
         return self._db[a_collection].find(a_filter)
@@ -94,67 +101,26 @@ class PyMongoStorageAdapter(IStorage):
 
         return w_res
 
-    async def up_sert(self, a_item, a_id, a_new_dict):
+    async def up_sert(
+        self,
+        a_collection: str,
+        a_id: str,
+        a_filter: Filter,
+        a_new_dict: t.Dict[str, t.Any],
+    ):
         """ " update or insert new dict"""
 
-        w_filter = {"_id": a_id, "_item_id": a_item["id"]}
+        return self._db[a_collection].update_one({"_id": a_id}, a_new_dict, upsert=True)
 
-        res = self._db[a_item["collection"]].find(w_filter)
-        count = self._db[a_item["collection"]].count_documents(w_filter)
-
-        if res != None and count != 0:
-            model = a_item["_class_obj"](res[0])
-            model._mongo_model = res[0]
-
-            if "_mongo_model" in a_new_dict:
-                a_new_dict["_mongo_model"]["_mat"] = time.time()
-                a_new_dict["_mongo_model"]["_item_id"] = a_item["id"]
-
-                w_update = {"$set": a_new_dict["_mongo_model"]}
-                model.update(a_new_dict)
-            else:
-                a_new_dict["_mat"] = time.time()
-                a_new_dict["_item_id"] = a_item["id"]
-
-                w_update = {"$set": a_new_dict}
-                model.update(a_new_dict)
-            if "_id" in w_update["$set"].keys():
-                del w_update["$set"]["_id"]
-            return self._db[a_item["collection"]].update_one(
-                w_filter, w_update, upsert=True
-            )
-        else:
-            if "_mongo_model" in a_new_dict:
-                a_new_dict["_mongo_model"]["_cat"] = time.time()
-                a_new_dict["_mongo_model"]["_mat"] = a_new_dict["_mongo_model"]["_cat"]
-                a_new_dict["_mongo_model"]["_item_id"] = a_item["id"]
-                a_new_dict["_mongo_model"]["_id"] = a_id
-
-                w_update = a_new_dict["_mongo_model"]
-
-            else:
-                a_new_dict["_id"] = a_id
-                a_new_dict["_mat"] = time.time()
-                a_new_dict["_cat"] = a_new_dict["_mat"]
-                a_new_dict["_item_id"] = a_item["id"]
-
-                w_update = a_new_dict
-            if "_id" not in w_update:
-                w_update["id"] = uuid4().__str__()
-
-            return await self._db[a_item["collection"]].insert_one(w_update)
-
-    async def up_sert_many(self, a_collection, a_filter, a_new_dict):
+    async def up_sert_many(
+        self,
+        a_collection: str,
+        a_filter: Filter,
+        a_list_dict: t.List[t.Dict[str, t.Any]],
+    ):
         """update or insert document with new dict regarding filter"""
-        if "_mongo_model" not in a_new_dict:
-            a_new_dict["_mongo_model"] = {}
-        a_new_dict["_mongo_model"]["_mat"] = time.time()
-        if "_mongo_model" in a_new_dict:
 
-            w_update = {"$set": a_new_dict["_mongo_model"]}
-        else:
-            w_update = {"$set": a_new_dict}
-        return self._db[a_collection].update_many(a_filter, w_update, upsert=True)
+        return self._db[a_collection].update_many(a_filter, a_list_dict, upsert=True)
 
     async def delete(self, a_collection, a_id):
         """delete document identified by id if it exists"""
--- src/unittest/python/test_storage_pymongo.py
+++ src/unittest/python/test_storage_pymongo.py
@@ -3,6 +3,7 @@ import pytest_asyncio
 from pymongo import MongoClient
 
 from ycappuccino.api.base import IActivityLogger, IConfiguration
+from ycappuccino.api.storage import Filter, Query
 from ycappuccino.storage.adapter.pymongo_storage_adapter import PyMongoStorageAdapter
 
 
@@ -36,55 +37,207 @@ class TestPyMongoStorage:
         )
 
     async def test_given_mongo_connected_and_collection_empty_get_one(self) -> None:
-
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        # When
+        result = await self.pymongo_storage_adapter.get_one("test")
+        # Then
+        assert result is None
 
     async def test_given_mongo_connected_and_collection_with_elem_get_one(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter={"name": "test"}, a_new_dict={"name": "test"}
+        )
+
+        # When
+        result = await self.pymongo_storage_adapter.get_one(
+            "test",
+            a_filter=None,
+            a_params=Query(offset=0, limit=1, sort=""),
+        )
+        #
+        # Then
+        assert result == {"name": "test"}
 
     async def test_given_mongo_connected_and_collection_with_elem_aggregate(
         self,
     ) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter={"name": "test"}, a_new_dict={"name": "test"}
+        )
+        # When
+        result = await self.pymongo_storage_adapter.aggregate(
+            a_collection="test", a_pipeline=[{"$match": {"name": "test"}}]
+        )
+
+        # Then
+        assert result == [{"name": "test"}]
 
     async def test_given_mongo_connected_and_collection_empty_aggregate(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        # When
+        result = await self.pymongo_storage_adapter.aggregate(
+            a_collection="test", a_pipeline=[{"$match": {"name": "test"}}]
+        )
+
+        # Then
+        assert result == []
 
     async def test_given_mongo_connected_and_no_collection_get_one(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        # When
+        result = await self.pymongo_storage_adapter.get_one("test")
+        # Then
+        assert result is None
 
     async def test_given_mongo_connected_and_collection_get_many(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter=None, a_new_dict={"name": "test"}
+        )
+        # When
+        result = await self.pymongo_storage_adapter.get_many(
+            "test",
+            a_filter=Filter(filter={"name": "test"}),
+            a_params=Query(offset=0, limit=1, sort=""),
+        )
+        # Then
+        assert result == [{"name": "test"}]
 
     async def test_given_mongo_connected_and_collection_list(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter=None, a_new_dict={"name": "test"}
+        )
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter=None, a_new_dict={"name": "test2"}
+        )
+        # When
+        result = await self.pymongo_storage_adapter.get_many(
+            "test",
+            a_filter=Filter(filter={}),
+            a_params=Query(offset=0, limit=2, sort=""),
+        )
+        # Then
+        assert len(result) == 2
+        assert result == [{"name": "test"}, {"name": "test2"}]
 
     async def test_given_mongo_connected_and_collection_insert_one(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        # When
+        result = await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter=Filter(filter={}), a_new_dict={"name": "test"}
+        )
+        # Then
+        assert result.acknowledged
+        res = await self.pymongo_storage_adapter.get_one(
+            "test", a_filter=Filter(filter={"name": "test"})
+        )
+        assert res == {"name": "test"}
 
     async def test_given_mongo_connected_and_collection_insert_many(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        # When
+        result = await self.pymongo_storage_adapter.up_sert_many(
+            a_collection="test",
+            a_filter=Filter(filter={}),
+            a_list_dict=[{"name": "test"}, {"name": "test2"}],
+        )
+        # Then
+        assert result.acknowledged
+        res = await self.pymongo_storage_adapter.get_one(
+            "test", a_filter=Filter(filter={"name": "test"})
+        )
+        assert res == {"name": "test", "name2": "test2"}
 
     async def test_given_mongo_connected_and_collection_delete_one(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter=None, a_new_dict={"name": "test"}
+        )
+        # When
+        result = await self.pymongo_storage_adapter.delete("test", "test")
+        # Then
+        assert result.acknowledged
+        res = await self.pymongo_storage_adapter.get_one("test")
+        assert res is None
 
     async def test_given_mongo_connected_and_collection_delete_many(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter=None, a_new_dict={"name": "test"}
+        )
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter=None, a_new_dict={"name": "test2"}
+        )
+        # When
+        result = await self.pymongo_storage_adapter.delete_many(
+            "test", Filter(filter={})
+        )
+        # Then
+        assert result.acknowledged
+        res = await self.pymongo_storage_adapter.get_many("test", Filter(filter={}))
+        assert res == []
 
     async def test_given_mongo_connected_and_collection_update_one(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter=None, a_new_dict={"name": "test"}
+        )
+        # When
+        result = await self.pymongo_storage_adapter.up_sert(
+            a_collection="test",
+            a_filter=Filter(filter={"name": "test"}),
+            a_new_dict={"name": "test2"},
+        )
+        # Then
+        assert result.acknowledged
+        res = await self.pymongo_storage_adapter.get_one(
+            "test", a_filter=Filter(filter={"name": "test2"})
+        )
+        assert res == {"name": "test2"}
 
     async def test_given_mongo_connected_and_collection_update_many(self) -> None:
 
-        pass
+        # Given
+        await self.pymongo_storage_adapter.connect()
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter=None, a_new_dict={"name": "test"}
+        )
+        await self.pymongo_storage_adapter.up_sert(
+            a_collection="test", a_filter=None, a_new_dict={"name": "test2"}
+        )
+        # When
+        result = await self.pymongo_storage_adapter.up_sert_many(
+            a_collection="test",
+            a_filter=Filter(filter={}),
+            a_list_dict=[{"name": "test3"}],
+        )
+        # Then
+        assert result.acknowledged
+        res = await self.pymongo_storage_adapter.get_many("test", Filter(filter={}))
+        assert res == [{"name": "test3"}, {"name": "test3"}]
